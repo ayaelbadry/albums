812b8130f78a22088e485c69182a5d1e96c6ad29
Mariam <mariam@Mariams-iMac.local>
2019-04-13 05:31:59 -0700

    axios

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './albumDetail';
 //import console = require('console');
  
 class AlbumList extends Component{
+    // initial a state component to update the component and show  the data 
+    state = {
+        albums: []
+    }
 
    // console.log("check the life cycle to app in React native");
     
     componentWillMount(){
-        console.log("Component will Mount Called")
+        console.log("Component will Mount Called");
+        console.log("load a json from this url using axios ");
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(res => this.setState({ albums: res.data}))
+        // U can using fetch Library rather than axios
+
+
+    }
+    //fuction to render a details from each albums 
+    renderAlbums(){
+        return this.state.albums.map(album => 
+        <AlbumDetail key = {album.title} album={album}/>)
     }
     render(){
+        console.log(this.state)
         return(
             <View>
-                <Text>AlbumList !!! </Text>
+                {this.renderAlbums()}
             </View>
         );
     }
